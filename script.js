582da2cc37747fe488b538a71d9cca97677148e8
Mauricio Lugo <maurogranados31@gmail.com>
2024-03-17 17:27:27 -0500

    Proyecto realizado

--- script.js
+++ script.js
@@ -1,75 +1,65 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const divContainer = document.getElementById('divContainer');
-    const button = document.getElementById('button');
+const divContainer = document.getElementById('divContainer');
+const button = document.getElementById('myButton');
 
-    for (let i = 0; i < 16; i++) {
-        const row = document.createElement('div');
-        row.classList.add('divRow');
+let size = 16;
 
-        for (let j = 0; j < 16; j++) {
-            const columns = document.createElement('div');
-            columns.classList.add('divItems');
-            row.appendChild(columns);
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('divRows');
 
-            columns.addEventListener('mouseenter', () => {
-                columns.style.backgroundColor = 'blue';
-            });
+    for (let j = 0; j < 16; j++) {
+        const items = document.createElement('div');
+        items.classList.add('divItems');
+        row.appendChild(items);
 
-            columns.addEventListener('mouseleave', () => {
-                columns.style.backgroundColor = '';
-            })
+        items.addEventListener('mouseenter', () => {
+            items.style.backgroundColor = 'red'
+        })
 
-        }
-
-        divContainer.appendChild(row);
+        divContainer.addEventListener('mouseleave', () => {
+            items.style.backgroundColor = '';
+        })
     }
 
-    let size = 16;
+    divContainer.appendChild(row);
+   
+}
 
-    button.addEventListener('click', () => {
-        let input = prompt('Type a new size(1 - 100):');
-        let newSize = parseInt(input);
-    
-        if (!isNaN(newSize) && newSize >= 1 && newSize <= 100) {
-            size = newSize;
-    
-            while (divContainer.firstChild) {
-                divContainer.removeChild(divContainer.firstChild);
-            }
-    
-            for (let i = 0; i < size; i++) {
-                const row = document.createElement('div');
-                row.classList.add('divRow');
-    
-                for (let j = 0; j < size; j++) {
-                    const columns = document.createElement('div');
-                    columns.classList.add('divItems');
-                    row.appendChild(columns);
-    
-                    columns.addEventListener('mouseenter', () => {
-                        columns.style.backgroundColor = 'blue';
-                    });
-    
-                    columns.addEventListener('mouseleave', () => {
-                        columns.style.backgroundColor = '';
-                    });
-                }
-    
-                divContainer.appendChild(row);
-            }
-    
-            const gridSize = size * 30; 
-            divContainer.style.width = gridSize + 'px';
-        } else {
-            alert('Type a valid number of 1 to 100.');
+button.addEventListener('click', () => {
+    textIn = prompt('Type a new size (1 - 100): ');
+    newSize = parseInt(textIn);
+
+    if (!isNaN(newSize) && newSize >= 1 && newSize <=100) {
+        size = newSize;
+
+        while (divContainer.firstChild) {
+            divContainer.removeChild(divContainer.firstChild);
         }
-    });
-    
-    
+
+        for (let i = 0; i < size; i++) {
+            const row = document.createElement('div');
+            row.classList.add('divRows');
+        
+            for (let j = 0; j < size; j++) {
+                const items = document.createElement('div');
+                items.classList.add('divItems');
+                row.appendChild(items);
+        
+                items.addEventListener('mouseenter', () => {
+                    items.style.backgroundColor = 'blue'
+                })
+        
+                divContainer.addEventListener('mouseleave', () => {
+                    items.style.backgroundColor = '';
+                })
+            }
+        
+            divContainer.appendChild(row);
+        }
+    } else {
+        alert('Type a valid number(1 - 100)!!')  
+    }
+
 });
 
-/*LISTO*/
-
-
-
-
+/*By Mauricio Lugo*/
